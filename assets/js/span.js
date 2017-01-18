fe3fdf7801ee64ec82784488c7fbd237951fdbc6
helloShen <symantec__@hotmail.com>
2017-01-17 19:31:24 -0500

    beta

--- assets/js/span.js
+++ assets/js/span.js
@@ -1,5 +1,6 @@
 $(window).load(function(){	//控制段落空行
-	$("h3").before("</br></br><hr>");
+	$("h3").before("</br></br>");
+	$("h3").after("<hr>");
 	$("h3").addClass("lead");
 	$("h4").before("</br>");
 })
